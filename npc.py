03c50e70f19376f2ef93da07af6185e4fa4bb0a4
aleeeksss-jr1 <aleksandar.jr1@gmail.com>
2026-04-25 21:23:34 +0200

    npc

--- npc.py
+++ npc.py
@@ -3,7 +3,7 @@ import animation
 from animation import animations
 
 class NPC(pygame.sprite.Sprite): # 'animation.AnimateSprite' est sensé être dans les parenthèses pour l'animation. Ainsi pour engendrer les boites de dialogues
-    def __init__(self, x, y):       # j'ai choisi de garder temporairement la classe Sprite. Lorsque l'animation sera requise oubliez pas de mettre l'animation dans le super
+    def __init__(self, x, y):    # j'ai choisi de garder temporairement la classe Sprite. Lorsque l'animation sera requise oubliez pas de mettre l'animation dans le super
         super().__init__()
 
         # l'image est sensée être animée, cependant en guise de solution temporaire, elle sera une surface seulement
@@ -13,3 +13,20 @@ class NPC(pygame.sprite.Sprite): # 'animation.AnimateSprite' est sensé être da
         self.rect = self.image.get_rect() 
         self.rect.x = x
         self.rect.y = y 
+
+        # couleur des dialogues
+        self.WHITE = (255, 255, 255) # -> texte
+        self.BROWN = (210, 105, 30) # -> fond
+
+    
+    def load_dialogue(filename, lines):
+        '''Méthode destinée à extraire le dialogue d'un fichier texte'''
+        with open(f'{filename}.txt', 'r') as f:
+            lignes = f.readlines(lines)
+    
+        return lignes
+    
+    def load_text_boxes(filename, lines):
+        '''Méthode destinée à créer des boites de dialogues'''
+        lignes = self.load_dialogue(filename, lines)
+        
